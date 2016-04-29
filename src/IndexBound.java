3fd33bf9c2b0a95bed9642718b794dc62d535bc9
srima karekal <srima589.karekal@gmail.com>
2016-04-29 12:01:21 +0530

    commit

---
+++ src/IndexBound.java
@@ -0,0 +1,15 @@
+
+public class IndexBound {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+String str ="somthing";
+System.out.println(str.substring(9));
+		}
+		catch(StringIndexOutOfBoundsException e){
+			System.out.println("fool you are deserving more than you have");
+		}
+	}
+
+}
